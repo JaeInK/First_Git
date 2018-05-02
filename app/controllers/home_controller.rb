571b371c773a8470d6e7bf7a8c440bec12495c84
JaeIn Kim <gjames5809@gmail.com>
2018-05-02 11:08:57 +0000

    Started Rails Project

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,20 @@
+class HomeController < ApplicationController
+  
+  def intro
+  end
+  
+  def a
+  end
+
+  def b
+  end
+
+  def c
+  end
+
+  def d
+  end
+
+  def e
+  end
+end
---
+++ test/controllers/home_controller_test.rb
@@ -0,0 +1,29 @@
+require 'test_helper'
+
+class HomeControllerTest < ActionController::TestCase
+  test "should get a" do
+    get :a
+    assert_response :success
+  end
+
+  test "should get b" do
+    get :b
+    assert_response :success
+  end
+
+  test "should get c" do
+    get :c
+    assert_response :success
+  end
+
+  test "should get d" do
+    get :d
+    assert_response :success
+  end
+
+  test "should get e" do
+    get :e
+    assert_response :success
+  end
+
+end
